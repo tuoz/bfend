2ca95881f49636bf22c7cba5d815a3330988d852
hypo <hypomail@gmail.com>
2018-05-14 17:16:04 +0800

    添加 ngc 编译

--- build.sh
+++ build.sh
@@ -2,10 +2,23 @@
 
 readonly currentDir=$(cd $(dirname $0); pwd)
 cd ${currentDir}
-rm -rf publish
 
-echo 'Copying lib'
-cp -r lib publish
+rm -rf publish
+rm -rf __gen_lib
+rm -rf publish-es2015
+
+cp -r lib __gen_lib
+node ./scripts/inline-template.js
+
+echo 'Compiling to es2015 via Angular compiler'
+$(npm bin)/ngc -p tsconfig-build.json -t es2015 --outDir publish-es2015/src
+
+mv publish-es2015/src publish
+
+echo 'Cleaning up temporary files'
+rm -rf __gen_lib
+
+echo 'Copying assets'
 cp -r assets publish/assets
 
 echo 'Copying package.json'
